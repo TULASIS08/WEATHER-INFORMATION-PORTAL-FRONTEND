3cf8464d21fc2b34197cbeb3ffdbcf12d384616f
TULASIS08 <tulasiaru2004@gmail.com>
2024-03-11 16:35:52 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://img.freepik.com/free-vector/flat-design-monsoon-season-clouds-illustration_23-2149424294.jpg?size=626&ext=jpg&ga=GA1.1.1700460183.1710115200&semt=ais" alt="Welcome picture" />
+    </div>
+  )
+}
